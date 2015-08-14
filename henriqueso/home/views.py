4f59aadaba4d77d650154029dbd7b966c210b04d
henriqueso <henrique.so@gmail.com>
2015-08-14 00:34:17 -0300

    Add first version of site

---
+++ henriqueso/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(
+		request,
+		'home/index.html',
+		{
+			"introduction" : {
+				"name" :"Henrique Oliveira", 
+				"text" : "Software Developer, Agile Enthusiast"
+			},
+			"links" : {
+				"twitter" : "https://twitter.com/henriqueso",
+				"github" : "https://github.com/henriqueso",
+				"linkedin" : "https://br.linkedin.com/in/henriqueso"
+			}
+		}
+	)
