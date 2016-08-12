20f6d8b47136003640d36798af1389195c44fd88
henriqueso <henrique.so@gmail.com>
2016-08-11 21:30:03 -0300

    fix path to static

--- henriqueso/settings.py
+++ henriqueso/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "henriqueso/static"),
 )
 
 # Simplified static file serving.
