908d3d52c09422c812e08643a56e87b4585471e9
Henrique Oliveira <henrique.so@gmail.com>
2022-10-25 22:01:47 -0300

    remove home folder

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
 	return render(
 		request,
-		'home/index.html',
+		'index.html',
 		{
 			"introduction" : {
 				"name" :"Henrique Oliveira", 
