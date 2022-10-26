265a91e495ff17ef5902df97ec4e32ac12734859
Henrique Oliveira <henrique.so@gmail.com>
2022-10-25 22:12:09 -0300

    viewa

--- home/views.py
+++ home/views.py
@@ -2,18 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(
-		request,
-		'index.html',
-		{
-			"introduction" : {
-				"name" :"Henrique Oliveira", 
-				"text" : "Software Developer, Agile Enthusiast"
-			},
-			"links" : {
-				"twitter" : "https://twitter.com/henriqueso",
-				"github" : "https://github.com/henriqueso",
-				"linkedin" : "https://br.linkedin.com/in/henriqueso"
-			}
-		}
-	)
+	return render(request, 'index.html')
