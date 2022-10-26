fd322a2f7bb529e2db328c88387a18517c74f775
Henrique Oliveira <henrique.so@gmail.com>
2022-10-25 21:23:54 -0300

    Update django structure

--- henriqueso/wsgi.py
+++ henriqueso/wsgi.py
@@ -1,17 +1,16 @@
 """
-WSGI config for henriqueso project.
+WSGI config for gettingstarted project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "henriqueso.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-
 application = get_wsgi_application()
-
